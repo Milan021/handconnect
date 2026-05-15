2b8edfb6190cfc393c09b0aa1536f070051d6b61
Milan Calic <milan.calic@joker-team.fr>
2026-05-15 14:49:58 +0200

    Ajout sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    { url: 'https://www.handballconnect.fr', lastModified: new Date() },
+    { url: 'https://www.handballconnect.fr/login', lastModified: new Date() },
+    { url: 'https://www.handballconnect.fr/register', lastModified: new Date() },
+    { url: 'https://www.handballconnect.fr/dashboard', lastModified: new Date() },
+    { url: 'https://www.handballconnect.fr/soutenir', lastModified: new Date() },
+  ]
+}
